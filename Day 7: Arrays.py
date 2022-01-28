1d19fcabd50cec1c81f6817161614ceb8ac95aa6
MuratErtugral <93777203+MuratErtugral@users.noreply.github.com>
2022-01-28 15:43:34 +0300

    Create Day 7: Arrays.py

---
+++ Day 7: Arrays.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+arr = reversed(arr)
+for m in arr:
+    print(m ,end=" ")
