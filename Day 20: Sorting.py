e2e7954ef9cd04ab1611c057b1e77601c944251f
MuratErtugral <93777203+MuratErtugral@users.noreply.github.com>
2022-02-19 21:15:46 +0300

    Create Day 20: Sorting.py

---
+++ Day 20: Sorting.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    # Write your code here
+    
+swap = 0
+while a != sorted(a):
+    for m in range(n-1):
+        if a[m]> a[m+1]:
+            swap += 1
+            a[m],a[m+1]=a[m+1],a[m]
+
+    
+
+    
+print("Array is sorted in {} swaps.".format(swap))
+print("First Element: {}".format(a[0]))
+print("Last Element: {}".format(a[-1]))
