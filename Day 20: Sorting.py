e29c5a1584be4fa34763808135646381b33a6e61
MuratErtugral <93777203+MuratErtugral@users.noreply.github.com>
2022-04-01 20:08:19 +0300

    Update Day 20: Sorting.py

--- Day 20: Sorting.py
+++ Day 20: Sorting.py
@@ -6,8 +6,6 @@ import random
 import re
 import sys
 
-
-
 if __name__ == '__main__':
     n = int(input().strip())
 
@@ -28,3 +26,6 @@ while a != sorted(a):
 print("Array is sorted in {} swaps.".format(swap))
 print("First Element: {}".format(a[0]))
 print("Last Element: {}".format(a[-1]))
+
+
+
