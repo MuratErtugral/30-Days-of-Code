252a3b2bc57ffda47167e3afa44aa756810e4870
MuratErtugral <93777203+MuratErtugral@users.noreply.github.com>
2022-05-09 15:16:18 +0300

    Update Day 9: Recursion 3.py

--- Day 9: Recursion 3.py
+++ Day 9: Recursion 3.py
@@ -1,17 +1,3 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'factorial' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER n as parameter.
-#
 
 def factorial(n):
     if n == 1 or n == 0:
