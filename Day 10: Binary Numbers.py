70eb7cc040f5f788854912e579f7c80bccd5b4df
MuratErtugral <93777203+MuratErtugral@users.noreply.github.com>
2022-05-10 23:57:17 +0300

    Update Day 10: Binary Numbers.py

--- Day 10: Binary Numbers.py
+++ Day 10: Binary Numbers.py
@@ -1,13 +1,3 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
-
 if __name__ == '__main__':
     n = int(input().strip())
 
@@ -29,3 +19,6 @@ if __name__ == '__main__':
             result = max(result,count)
     
     print(result)
+
+    
+    
