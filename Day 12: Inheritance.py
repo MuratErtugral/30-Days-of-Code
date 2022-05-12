dbb9736c258975878f3aedb16db0e5e4a5c12980
MuratErtugral <93777203+MuratErtugral@users.noreply.github.com>
2022-05-12 12:19:48 +0300

    Update Day 12: Inheritance.py

--- Day 12: Inheritance.py
+++ Day 12: Inheritance.py
@@ -2,21 +2,8 @@
 
         
 class Student(Person):
-    #   Class Constructorsdfsdf
-    #   
-    #   Parameters:
-    #   firstName - A string denoting the Person's first name.
-    #   lastName - A string denoting the Person's last name.
-    #   id - An integer denoting the Person's ID number.
-    #   scores - An array of integers denoting the Person's test scores.
-    #
-    # Write your constructor here
+  
     
-
-    #   Function Name: calculate
-    #   Return: A character denoting the grade.
-    #
-    # Write your function here
     def __init__(self,firstName, lastName, idNum , scores):
 
         self.firstName = firstName
